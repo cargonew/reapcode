6ef63ce0932803905ba63f9e5907dca9f96a2642
yourfather <okuhlexesibe0@gmail.com>
2025-05-24 22:24:11 +0200

    optimized solution for  problem 2656

--- medium/Maximum-sum.rs
+++ medium/Maximum-sum.rs
@@ -1,18 +1,11 @@
-impl Solution {
-    pub fn maximize_sum( mut nums: Vec<i32>, k: i32) -> i32 {
+pub fn maximize_sum(nums: Vec<i32>, k: i32) -> i32 {
+        
+        let mut max_val = *nums.iter().max().unwrap();
+        let mut maxScore = 0 ;
 
-        nums.sort();
-
-        let mut score : i32  = 0 ;
-
-        let n = nums.len()-1;
-
-        for i in 1..=k {
-
-            score +=nums[ n as usize] ; 
-
-            nums[n as usize] = nums[n as usize]+1;
+        for i in 0..k{
+            maxScore += max_val;
+            max_val += 1 ;
         }
-        score
+        maxScore
     }
-}
